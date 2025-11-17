e7c02b7cca9b61ce1e19b4c5a65b33379c8dc289
Arun joshi <arungoback123@gmail.com>
2025-11-18 01:36:04 +0545

    quize url set

--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -44,8 +44,8 @@ const data = {
       icon: IconListDetails,
     },
     {
-      title: "Analytics",
-      url: "/admin/analytics",
+      title: "Quizzes",
+      url: "/admin/quizzes",
       icon: IconChartBar,
     },
     {
